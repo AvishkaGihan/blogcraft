99560478782825912854364c63912d1068e22126
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2024-09-16 22:10:22 +0530

    Create the project structure and Update next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+};
 
-export default nextConfig;
+module.exports = nextConfig;
